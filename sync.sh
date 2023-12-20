bec702f2b692377a028b3351e4bb554d6bba925e
David Hanna <david.hanna@kickstepinc.com>
2023-12-20 11:57:01 -0500

    fixed a hard-coded ARN value

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-AWS_PROFILE=ks /usr/local/bin/aws s3 sync . s3://kickstep-cf-templates/${1} --include "*" --exclude ".git/**"
+AWS_PROFILE=ks aws s3 sync . s3://kickstep-cf-templates/${1} --include "*" --exclude ".git/**"
